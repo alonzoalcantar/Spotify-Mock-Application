36678912d738a1d03f497ceb204541bfa9ae5645
alonzoalcantar <pinoa.03@outlook.com>
2022-12-02 00:42:26 -0700

    Sign up data displays in console

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+
+
 // Put all API routes here (before the catch-all)
+
+app.use('/api/users', require('./routes/api/users'));
 
 // "catch-all" route that will match all GET requests
 // that don't match an API route defined above

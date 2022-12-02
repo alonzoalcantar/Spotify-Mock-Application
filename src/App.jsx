aa1778af35d10dd7c1beed34cab0c45b71a3b78e
alonzoalcantar <pinoa.03@outlook.com>
2022-12-01 21:32:29 -0700

    Deleted default files, Set home page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Home Page
+      </header>
+    </div>
+  );
+}
+
+export default App;

b3a931f70a1fd522fa6496ce11df99892e3bf47a
alonzoalcantar <pinoa.03@outlook.com>
2022-12-09 10:24:19 -0700

    Songs play immedietly once selected

--- src/Components/TrackPlayer/TrackPlayer.jsx
+++ src/Components/TrackPlayer/TrackPlayer.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react"
 import SpotifyWebPlayer from "react-spotify-web-playback"
 
 export default function TrackPlayer({accessToken, trackUri}) {
+
+    const [play, setPlay] = useState(false)
+
+    useEffect(() => {
+        setPlay(true)
+    }, [trackUri])
+
     if(!accessToken) return null
     return(
         <div><SpotifyWebPlayer
         token={accessToken}
         showSaveIcon
+        callback={state => {
+            if(!state.isPlaying) setPlay(false)
+        }}
+        play={play}
         uris={trackUri ? [trackUri] : []}
+
         />
         
         </div>

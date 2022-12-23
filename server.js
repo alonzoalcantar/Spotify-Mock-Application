0ed19bec781494ccd7729554162d96d63af82675
alonzoalcantar <pinoa.03@outlook.com>
2022-12-23 15:05:57 -0700

    redirect uri refactor

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/callback', (req,res) => {
 
             // Redirect to REACT APP
 
-            res.redirect(`http://localhost:3000/?${queryParams}`)
+            res.redirect(`https://thawing-bayou-17998.herokuapp.com/?${queryParams}`)
     } else {
         res.redirect(`/?${querystring.stringify({ error: 'invalid_token'})}`);
     }
